7e2bc2cb0c5702708be7b59fd600dbc565280c2a
Lutfor Rahman <lutfor.raju@sjinnovation.com>
2021-09-04 21:11:06 +0600

    font added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
     return (
-        <>
-          I'm Home 
-        </>
+      <div className="wrapper fadeInDown">
+      <div id="formContent" className="pt-5">
+        <form>
+          <input type="email" id="email" className="fadeIn second" name="email" placeholder="Email"/>
+          <input type="password" id="password" className="fadeIn third" name="password" placeholder="password"/>
+          <input type="submit" className="fadeIn fourth" value="Log In"/>
+        </form>
+        <div id="formFooter">
+          {/* <a className="underlineHover" href="/">Forgot Password?</a> */}
+        </div>
+    
+      </div>
+    </div>
     )
 }
 
